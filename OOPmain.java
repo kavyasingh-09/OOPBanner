17c80037d3e82924fae6c2ce3980b04ee3c4748c
kavya <ks9677@srmist.edu.in>
2026-02-18 00:23:11 +0530

    start Made main

---
+++ OOPmain.java
@@ -0,0 +1,15 @@
+// This is Main branch
+
+/*
+This is Main branch
+
+@version 1.0
+@author kavyaa
+
+*/
+
+class OOPmain{
+    public static main(String args[]){
+        system.out.println("This is main branch");
+    }
+}
