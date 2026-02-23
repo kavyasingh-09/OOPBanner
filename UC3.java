ecc8a16c3adb079cc5b102b84fb246fee1098e7a
kavyasingh-09 <kavyaa.official09@gmail.com>
2026-02-23 10:00:49 +0530

    UC3 ADDED

---
+++ UC3.java
@@ -0,0 +1,14 @@
+class UC3 {
+    public static void main(String[] args) {
+
+        String oops = String.join("\n",
+                " *****   *****   *****   ***** ",
+                "*     * *     * *     * *      ",
+                "*     * *     * * * * *  ***** ",
+                "*     * *     * *            * ",
+                " *****   *****  *        ***** "
+        );
+
+        System.out.println(oops);
+    }
+}
